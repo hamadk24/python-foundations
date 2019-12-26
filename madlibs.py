5fe4b7cbe876bbef8ad432ba9329add7c345f557
hamadk24 <hamadalkhaled24@gmail.com>
2019-12-26 16:39:16 +0300

    Madlibs

--- madlibs.py
+++ madlibs.py
@@ -7,4 +7,4 @@ name = input("Enter a name: ")
 scream = input("Enter any sentence: ")
 action = input("Enter a verb: ")
 
-print("The story goes...\nIt was {0} when I heard a knock at the door.\nI opened the door and there was a box full of {1} with a note saying \"From {2}.\"\nJust as I closed the door I heard a scream: \"{3}.\"\n I froze in place and all I could do was {4}.").format(time, items, name.title(), scream.upper(), action)
+print("The story goes...\nIt was {0} when I heard a knock at the door.\nI opened the door and there was a box full of {1} with a note saying \"From {2}.\"\nJust as I closed the door I heard a scream: \"{3}.\"\n I froze in place and all I could do was {4}.".format(time, items, name.title(), scream.upper(), action))
