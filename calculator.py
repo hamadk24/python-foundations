e049ece8a1be5f531c17086bb17e5582aac71dea
hamadk24 <hamadalkhaled24@gmail.com>
2019-12-29 14:40:53 +0300

    Calculator

---
+++ calculator.py
@@ -0,0 +1,22 @@
+first_number = (input("Enter the first number: "))
+second_number = (input("Enter the second number: "))
+operation = input("Choose the operation(+, -, /, *): ")
+
+if operation == '+':
+    print('{} + {} = '.format(first_number, second_number))
+    print(first_number + second_number)
+
+elif operation == '-':
+    print('{} - {} = '.format(first_number, second_number))
+    print(first_number - second_number)
+
+elif operation == '*':
+    print('{} * {} = '.format(first_number, second_number))
+    print(first_number * second_number)
+
+elif operation == '/':
+    print('{} / {} = '.format(first_number, second_number))
+    print(first_number / second_number)
+
+else:
+    print('You have not typed a valid operator or number, please run the program again.')
